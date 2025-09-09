b9f25a303f68fa26df83dd513f77315c9f999117
lucasxavila <lucasguilhermeab02@gmail.com>
2025-09-09 16:49:35 -0300

    Add project files.

---
+++ ExercicioUm.cs
@@ -0,0 +1,14 @@
+namespace fundamentosCsharp;
+
+class ExercicioUm
+{
+    public void Executar()
+    {
+        Console.WriteLine("Digite seu primeiro nome: ");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine($"0lá, {nome}! Seja muito bem-vindo!");
+
+        Console.ReadKey();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace fundamentosCsharp;
+
+class Program
+{
+    static void Main()
+    {
+        var exercicio1 = new ExercicioUm();
+        exercicio1.Executar();
+    }
+}
